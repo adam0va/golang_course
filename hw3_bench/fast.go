95e62fa4253288f45e8a574e3bf65123c265754d
Irina <irinaadamova555@gmail.com>
2020-07-20 21:39:26 +0300

    solves third task

---
+++ hw3_bench/fast.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"bufio"
+	//"encoding/json"
+	"fmt"
+	"io"
+	//"io/ioutil"
+	"os"
+	//"regexp"
+	"strings"
+	//"log"
+)
+
+import (
+	json "encoding/json"
+	easyjson "github.com/mailru/easyjson"
+	jlexer "github.com/mailru/easyjson/jlexer"
+	jwriter "github.com/mailru/easyjson/jwriter"
+)
+
+// suppress unused package warning
+var (
+	_ *json.RawMessage
+	lexer *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
+)
+
+type User struct {
+	Browsers []string `json:"browsers"`
+	Company  string   `json:"-"`
+	Country  string   `json:"-"`
+	Email    string   `json:"email"`
+	Job      string   `json:"-"`
+	Name     string   `json:"name"`
+	Phone    string   `json:"-"`
+}
+
+//const filePath string = "./data/users.txt"
+
+func easyjson9e1087fdDecodeHw3Bench(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeFieldName(false)
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "email":
+			out.Email = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson9e1087fdDecodeHw3Bench(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson9e1087fdDecodeHw3Bench(l, v)
+}
+
+func FastSearch(out io.Writer) {
+	file, err := os.Open(filePath) 
+	if err != nil {
+		panic(err)
+	}
+
+	seenBrowsers := []string{}
+	uniqueBrowsers := 0
+	foundUsers := ""
+
+	i := 0 
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		user := User{}
+
+		user.UnmarshalJSON([]byte(line))
+		//log.Println(user)
+		//users = append(users, user)
+
+		isAndroid := false
+		isMSIE := false
+
+		for _, browser := range user.Browsers {
+			if ok := strings.Contains(browser, "Android"); ok {
+				isAndroid = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}  
+			if ok := strings.Contains(browser, "MSIE"); ok {
+				isMSIE = true
+				notSeenBefore := true
+				for _, item := range seenBrowsers {
+					if item == browser {
+						notSeenBefore = false
+					}
+				}
+				if notSeenBefore {
+					seenBrowsers = append(seenBrowsers, browser)
+					uniqueBrowsers++
+				}
+			}
+		}
+		i++
+		if !(isAndroid && isMSIE) {
+			continue
+		}
+
+		email := strings.ReplaceAll(user.Email, "@", " [at] ")
+		foundUsers += fmt.Sprintf("[%d] %s <%s>\n", i-1, user.Name, email)
+	}
+
+	fmt.Fprintln(out, "found users:\n"+foundUsers)
+	fmt.Fprintln(out, "Total unique browsers", len(seenBrowsers))
+}
